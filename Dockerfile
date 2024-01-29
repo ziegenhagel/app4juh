e26c5f5071845e90adcbd04d2e9f4f832e54642a
Dominik <dominik.ziegenhagel@gmail.com>
2024-01-29 23:08:52 +0100

    change dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
-FROM node:17-alpine
+# Use any Node.js base image that you want (as long as it's Alpine)!
+FROM node:18-alpine
 
-RUN mkdir -p /usr/src/nuxt-app
-WORKDIR /usr/src/nuxt-app
-COPY . .
+# Set the working directory to /app
+WORKDIR /app
 
-RUN npm
+# Copy the package.json file into the working directory before copying the rest of the files to cache the dependencies
+COPY package.json /app
+
+# Install the dependencies, you might want to use yarn or pnpm instead
+RUN npm install
+
+# Copy the rest of the files into the working directory
+COPY . /app
+
+# Build the application, again, use yarn or pnpm if you want
 RUN npm run build
 
-ENV NUXT_HOST=0.0.0.0
-ENV NUXT_PORT=3000
+EXPOSE 3000/tcp
 
-EXPOSE 3000 
-
-ENTRYPOINT ["node", ".output/server/index.mjs"]
+# Start the application. This is the default command for Nuxt 3
+CMD ["node", ".output/server/index.mjs"] 
--- app.vue
+++ app.vue
@@ -3,14 +3,19 @@
     <v-app-bar flat>
       <img src="/image/app4juh_blau.svg" alt="App4Juh Logo" class="w-32 h-32 mx-auto"/>
     </v-app-bar>
-    <v-app-bar flat class="-mt-3">
-      <v-tabs v-model="activeTab" grow color="#EB003C">
+    <v-app-bar
+        flat class="-mt-3">
+      <v-tabs
+          class="mx-auto max-w-6xl"
+          v-model="activeTab" grow color="#EB003C">
         <v-tab v-for="tab in tabs" :key="tab">{{ tab }}</v-tab>
       </v-tabs>
     </v-app-bar>
-    <v-main class="bg-gray-100">
-
-    </v-main>
+    <v-container>
+      <v-main>
+        <apps v-if="activeTab === 0"/>
+      </v-main>
+    </v-container>
   </v-app>
 </template>
 <script setup lang="ts">
@@ -18,6 +23,10 @@ const tabs = ['Apps', 'Dateien', 'Ideen']
 const activeTab = ref(tabs[0])
 </script>
 <style>
+.v-application__wrap {
+  @apply bg-gray-100;
+}
+
 .v-slide-group {
   height: 3em;
   margin-bottom: -1em;
