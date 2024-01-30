89b33ad53033018ae566e4a982a7d727eb0e9371
Dominik <dominik.ziegenhagel@gmail.com>
2024-01-30 11:39:37 +0100

    web app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,15 @@ export default defineNuxtConfig({
             {charset: "utf-8"},
             {name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"},
             {hid: "description", name: "description", content: "Apps für die Erste Hilfe Kurse der JUH"},
+            // full web app ios and android
+            {name: "mobile-web-app-capable", content: "yes"},
+            {name: "apple-mobile-web-app-capable", content: "yes"},
+            {name: "apple-mobile-web-app-status-bar-style", content: "black"},
+            {name: "apple-mobile-web-app-title", content: "App4Juh"},
+            // favicon
+            {name: "msapplication-TileImage", content: "/image/app4juh.jpg"},
+
+
         ],
         link: [{rel: "icon", type: "image/x-icon", href: "/image/app4juh.jpg"}],
     },
