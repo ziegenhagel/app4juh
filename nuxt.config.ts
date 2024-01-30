30efa7b1a822175ab525af2569372fc3863bc3fd
Dominik <dominik.ziegenhagel@gmail.com>
2024-01-30 11:38:11 +0100

    add logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
     devtools: {enabled: true},
     modules: ["@nuxtjs/tailwindcss", "nuxt-vuetify", "@nuxt/image"],
-    ssr: false
+    ssr: false,
+    /* have favicon and mobile web app capable and title App4Juh*/
+    head: {
+        title: "App4JUH",
+        meta: [
+            {charset: "utf-8"},
+            {name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"},
+            {hid: "description", name: "description", content: "Apps für die Erste Hilfe Kurse der JUH"},
+        ],
+        link: [{rel: "icon", type: "image/x-icon", href: "/image/app4juh.jpg"}],
+    },
+
+
 })
