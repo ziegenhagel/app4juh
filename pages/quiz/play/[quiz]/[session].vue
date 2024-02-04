d27b93d67481fd3072ca0fd237c81dfefd4e6447
Dominik <dominik.ziegenhagel@gmail.com>
2024-02-04 10:17:19 +0100

    quizze funktionieren jetzt

--- pages/quiz/play/[quiz]/[session].vue
+++ pages/quiz/play/[quiz]/[session].vue
@@ -1,3 +1,146 @@
 <template>
-  Client view
+  <div class="p-4">
+    <template v-if="question">
+      <h1 class="text-3xl"><b>Frage {{ questionIndex + 1 }}</b> {{ question.title }}</h1>
+      <div class="flex flex-col gap-4 mt-8" :class="status === 'waiting' ? 'opacity-50 pointer-events-none' : ''">
+        <div class="answer overflow-hidden" v-for="(answer, index) in question.answers" :key="index"
+             @click="clickAnswer(index)"
+             :class="status === 'show_solution' && answer.correct ? 'bg-green-600/90 text-white' : 'bg-white'">
+          <div class="p-4">
+            <div class="w-8 d-inline-block font-bold">{{ ['A', 'B', 'C', 'D', 'E', 'F'][index] }}</div>
+            {{ answer.title }}
+          </div>
+          <div class="progress-wrapper" v-if="status === 'show_solution'">
+            <div :style="{width: answer.clicks * 100 / question.clicks + '%'}"
+                 class="h-1 -mt-1 transition-all duration-1000 ease-in-out"
+                 :class="answer.correct ? 'bg-green-700' : 'bg-[#010545]'"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </template>
+    <div v-else-if="questionIndex>1"
+         class="h-full absolute inset-0 w-full flex-col flex justify-center items-center bg-gray-100">
+      <h1 class="text-4xl">Quiz beendet</h1>
+    </div>
+    <div v-else
+         class="h-full absolute inset-0 w-full flex-col flex justify-center items-center bg-gray-100">
+
+      <div v-if="presenterAsk" class="p-8 flex flex-col gap-4">
+
+        <h1 class="text-4xl">Spielmodus</h1>
+        <h1 class="text-xl">Möchten Sie als Dozent oder als Teilnehmer:in spielen?</h1>
+        <v-btn :to="'/quiz/play/' + route.params.quiz + '?session='+ route.params.session" prepend-icon="mdi-play">Als
+          Dozent
+        </v-btn>
+        <v-btn @click="presenterAsk = false" class="mb-12" prepend-icon="mdi-play">Als Teilnehmer:in</v-btn>
+      </div>
+
+      <h1 class="text-4xl" v-else>Bitte warten</h1>
+    </div>
+    <img src="/logo.svg" alt="icon" class="icon"
+         style="position:fixed;width:150px;left:50%;margin-left:-75px;bottom:25px">
+  </div>
 </template>
+<script setup>
+import {ID, Query} from 'appwrite'
+
+definePageMeta({layout: 'quiz'})
+const route = useRoute()
+const appwrite = useAppwrite()
+
+// GET QUIZ
+const res = await appwrite.database.listDocuments('quiz', 'quizzes', [
+  Query.equal('$id', route.params.quiz)
+])
+
+
+// Schauen nach quiz+session in den clicks, ob schon ein has_presenter = true ist
+const presenter = await appwrite.database.listDocuments('quiz', 'clicks', [
+  Query.equal('quiz', res.documents[0].$id),
+  Query.equal('session', route.params.session),
+  Query.equal('has_presenter', true)
+])
+
+console.log('presenter', presenter.total)
+const presenterAsk = ref(presenter.total === 0)
+
+// SETUP QUIZ
+let quizTmp = res.documents[0]
+quizTmp.questions = JSON.parse(quizTmp.questions)
+quizTmp.questions.forEach(question => {
+  question.answers.forEach(answer => {
+    answer.clicks = 0
+  })
+  question.clicks = 0
+})
+const quiz = ref(quizTmp)
+const questionIndex = ref(-1)
+const question = computed(() => quiz.value.questions?.[questionIndex.value])
+
+// ZUSTAND VARIABLEN
+const status = ref('show_question') // show_solution, waiting
+const finished = ref(false)
+
+// ACTIONS
+const clickAnswer = async (answer) => {
+  console.log('clickAnswer', answer)
+  if (status.value !== 'show_question') return
+
+  status.value = 'waiting'
+  await appwrite.database.createDocument('quiz', 'clicks',
+      ID.unique(),
+      {
+        quiz: quiz.value.$id,
+        session: route.params.session,
+        client_freigabe: false, // nein, wir sind ja selbst der client. client_freigabe auf der aktuellen questionIndex setzt status auf show_solution
+        questionIndex: questionIndex.value,
+        choiceIndex: answer
+      })
+}
+
+
+const setupAppwriteClickListener = () => {
+  console.log('setupAppwriteClickListener')
+  client.subscribe('databases.quiz.collections.clicks.documents', async (event) => {
+    if (event.payload.session === route.params.session && event.payload.quiz === quiz.value.$id) {
+      if (event.payload.client_freigabe) {
+
+        if (event.payload.questionIndex === questionIndex.value) {
+          status.value = 'show_solution'
+        } else {
+          // if questionIndex is higher, show next question
+          questionIndex.value++
+          status.value = 'show_question'
+        }
+
+        // START QUIZ
+        if (questionIndex.value === -1) {
+          questionIndex.value = 0
+          status.value = 'show_question'
+        }
+      } else {
+        // in the current quiz, update the question and answer clicks
+        quiz.value.questions[event.payload.questionIndex].clicks++
+        quiz.value.questions[event.payload.questionIndex].answers[event.payload.choiceIndex].clicks++
+      }
+    }
+    console.log('click', event)
+  })
+}
+
+const client = useAppwriteClient()
+
+onMounted(setupAppwriteClickListener)
+</script>
+<style>
+
+h1, h2 {
+  color: #010545
+}
+
+.answer {
+  color: #010545;
+  @apply rounded-lg text-3xl shadow-lg
+}
+</style>
--- pages/quiz/play/[quiz]/index.vue
+++ pages/quiz/play/[quiz]/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div v-if="ready" @click="next"
        class="h-full absolute inset-0 flex-col flex bg-gray-100 px-10 py-12 my-3 cursor-pointer">
-    <h1 class="text-5xl">
+    <h1 class="text-4xl">
       <b>Frage {{ questionIndex + 1 }}</b>
       {{ question.title }}</h1>
-    <div class="flex flex-col gap-4 mt-16">
+    <div class="flex flex-col gap-4 mt-8">
       <div class="answer overflow-hidden" v-for="(answer, index) in question.answers" :key="index"
            :class="status === 'show_solution' && answer.correct ? 'bg-green-600/90 text-white' : 'bg-white'">
-        <div class="p-6">
+        <div class="p-4">
           <div class="w-8 d-inline-block font-bold">{{ ['A', 'B', 'C', 'D', 'E', 'F'][index] }}</div>
           {{ answer.title }}
         </div>
@@ -21,56 +21,63 @@
     </div>
   </div>
   <div v-else-if="finished"
-       class="h-full absolute inset-0 w-full flex-col flex justify-center items-center bg-gray-300">
+       class="h-full absolute inset-0 w-full flex-col flex justify-center items-center bg-gray-100">
     <h1 class="text-4xl">Quiz beendet</h1>
   </div>
-  <div v-else class="h-full absolute inset-0 w-full flex-col flex justify-center items-center bg-gray-300">
-    <div class="w-64 bg-white shadow-lg overflow-hidden rounded-lg">
+  <div v-else class="h-full absolute inset-0 w-full flex-col flex justify-center items-center bg-gray-100">
+    <div v-if="showQr" class="w-64 bg-white shadow-lg overflow-hidden rounded-lg">
       <img :src="urlQR" class="w-64 h-64 object-cover"/>
       <div class="-mt-4 text-center w-full mx-auto text-mono pb-3 text-lg">{{ session }}</div>
       <div class="w-64">
         <v-progress-linear indeterminate color="#EB003C"></v-progress-linear>
       </div>
     </div>
+    <h1 class="text-4xl" v-else>{{ quiz.title }}</h1>
     <v-btn
-        @click="next" class="my-8" prepend-icon="mdi-play">Quiz starten
+        @click="next" class="my-8" prepend-icon="mdi-play">Starten!
     </v-btn>
   </div>
 
   <!-- CI -->
-  <img src="/icon.svg" alt="icon" class="icon" style="position:fixed;width:55px;left:0;bottom:0;margin:40px">
-  <div class="aus-liebe-zum-leben">Aus Liebe zum Leben</div>
+  <img src="/logo.svg" alt="icon" class="icon"
+       style="position:fixed;width:150px;left:50%;margin-left:-75px;bottom:25px">
+  <!--  <img src="/icon.svg" alt="icon" class="icon" style="position:fixed;width:55px;left:0;bottom:0;margin:40px">-->
+  <!--  <div class="aus-liebe-zum-leben">Aus Liebe zum Leben</div>-->
 </template>
 <script setup>
 import {ID, Query} from 'appwrite'
 
 const route = useRoute()
 const appwrite = useAppwrite()
-const session = Math.random().toString(36).substring(7)
+const client = useAppwriteClient()
+const session = route.query.session || Math.random().toString(36).substring(7)
+const showQr = ref(route.query.session === undefined)
 const res = await appwrite.database.listDocuments('quiz', 'quizzes', [
   Query.equal('$id', route.params.quiz)
 ])
-let quiz = res.documents[0]
+let quizTmp = res.documents[0]
 
 //  quiz questions must be json parsed
-quiz.questions = JSON.parse(quiz.questions)
+quizTmp.questions = JSON.parse(quizTmp.questions)
 
 // each question has a total count of clicks, and each answer has a count of clicks
-quiz.questions.forEach(question => {
+quizTmp.questions.forEach(question => {
   question.answers.forEach(answer => {
     answer.clicks = 0
   })
   question.clicks = 0
 })
 
+const quiz = ref(quizTmp)
+
 definePageMeta({layout: 'quiz'})
 const ready = ref(false)
-const url = computed(() => window.location.origin + '/quiz/play/' + quiz.$id + '/' + session)
+const url = computed(() => window.location.origin + '/quiz/play/' + quiz.value.$id + '/' + session)
 const urlQR = computed(() => `https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${url.value}`)
 const clearQuiz = async () => {
   // get all clicks from that quiz and session
   const res = await appwrite.database.listDocuments('quiz', 'clicks', [
-    Query.equal('quiz', quiz.$id),
+    Query.equal('quiz', quiz.value.$id),
     Query.equal('session', session)
   ])
   // delete all from that quiz and session
@@ -78,7 +85,7 @@ const clearQuiz = async () => {
   ready.value = true
 }
 const questionIndex = ref(-1)
-const question = computed(() => quiz.questions[questionIndex.value])
+const question = computed(() => quiz.value.questions?.[questionIndex.value])
 const finished = ref(false)
 const status = ref('show_solution')
 const next = async () => {
@@ -86,29 +93,75 @@ const next = async () => {
   if (status.value === 'show_solution') {
     // Die User dürfen nun die nächste Frage anklicken
     questionIndex.value++
-    if (questionIndex.value < quiz.questions.length) {
+    if (questionIndex.value < quiz.value.questions.length) {
       status.value = 'show_question'
-      await appwrite.database.createDocument('quiz', 'clicks',
-          ID.unique(),
-          {
-            quiz: quiz.$id,
-            session,
-            client_freigabe: true,
-            questionIndex: questionIndex.value,
-          })
-      console.log('next', questionIndex.value)
+      // await appwrite.database.createDocument('quiz', 'clicks',
+      //     ID.unique(),
+      //     {
+      //       quiz: quiz.value.$id,
+      //       session,
+      //       client_freigabe: true,
+      //       questionIndex: questionIndex.value,
+      //     })
+      // console.log('next', questionIndex.value)
     } else {
       ready.value = false
       finished.value = true
+
+      // jetzt alle clicks zu dieser session löschen
+      const res = await appwrite.database.listDocuments('quiz', 'clicks', [
+        Query.equal('session', session)
+      ])
+      console.log('res', res)
+
     }
   } else {
     // wir zeigen die Frage
     status.value = 'show_solution'
+
   }
+
+  await appwrite.database.createDocument('quiz', 'clicks',
+      ID.unique(),
+      {
+        quiz: quiz.value.$id,
+        session,
+        client_freigabe: true,
+        questionIndex: questionIndex.value,
+      })
 }
 
+// listen to clicks
+const setupAppwriteClickListener = () => {
+  console.log('setupAppwriteClickListener')
+  client.subscribe('databases.quiz.collections.clicks.documents', async (event) => {
+    if (event.payload.session === session && event.payload.quiz === quiz.value.$id) {
+      // in the current quiz, update the question and answer clicks
+      quiz.value.questions[event.payload.questionIndex].clicks++
+      quiz.value.questions[event.payload.questionIndex].answers[event.payload.choiceIndex].clicks++
+    }
+    console.log('click', event)
+  })
+}
+
+const setPresenter = async () => {
+  console.log('setPresenter')
+  await appwrite.database.createDocument('quiz', 'clicks',
+      ID.unique(),
+      {
+        quiz: quiz.value.$id,
+        session,
+        has_presenter: true,
+        questionIndex: -1,
+        choiceIndex: -1
+      })
+  console.log('setPresenter')
+}
+
+setPresenter()
 onMounted(() => {
   clearQuiz()
+  setupAppwriteClickListener()
   // next()
 })
 </script>
@@ -129,6 +182,6 @@ h1, h2 {
 
 .answer {
   color: #010545;
-  @apply rounded-lg text-3xl shadow-lg
+  @apply rounded-lg text-2xl shadow-lg
 }
 </style>
