89b4e981dd088e83a387be51e9a43c728616c502
Dominik <dominik.ziegenhagel@gmail.com>
2024-02-15 17:23:19 +0100

    mit api

--- composables/inseln.ts
+++ composables/inseln.ts
@@ -1,34 +1,42 @@
 export const inseln = [
     {
         name: 'Hafen',
-        color: '#E4003A'
+        color: '#E4003A',
+        fall:false
     },
     {
         name: 'Nicht erweckbar',
+        fall: 'Vergiftung,Alkoholvergiftung,Unterkühlung,Zn Krampfanfall,Helmabnahme',
         color: '#EFBC3A'
     },
     {
         name: 'Keine Atmung',
+        fall: 'Atemstillstand,Ertrinkung,Stromunfall,Unterkühlung,Vergiftung',
         color: '#B2B2B1'
     },
     {
         name: 'Probleme in der Brust',
+        fall: 'Herzinfarkt,Verschluckter Gegenstand,Asthma Anfall, Allergische Reaktion',
         color: '#6AB2E3'
     },
     {
         name: 'Verletzungen',
+        fall: 'Knochenbruch,Verbrennung,Verkühlung,Ampuation,Sturz,Kopfverletzung,Schnittverletzung,Sportverletzung,Druckverband,Zahn Unfall',
         color: '#F0833D'
     },
     {
         name: 'Probleme im Kopf',
+        fall: 'Schlaganfall,Hitzschlag,Sonnenstich',
         color: '#C797C4'
     },
     {
         name: 'Probleme im Bauch',
+        fall: 'Vergiftung,Unterzucker,Überzucker,Alkoholvergiftung',
         color: '#8EC36F'
     },
     {
         name: 'Vorbeugen',
+        fall:false,
         color: '#E4003A'
     }
 ]
---
+++ pages/fall.vue
@@ -0,0 +1,204 @@
+<template>
+  <div>
+    <v-app-bar color="#eb003c" dark>
+      <img src="/image/app4juh.svg" alt="App4Juh Logo" class="w-32"/>
+      <v-app-bar-title>
+        <span class="font-bold">Fallbeispiel</span>
+      </v-app-bar-title>
+    </v-app-bar>
+    <main class="max-w-lg mx-auto px-4 mt-10">
+
+      <template v-if="step==0">
+        <h1 class="text-4xl mb-6">Fallbeispiele erzeugen</h1>
+        <h2 class="text-2xl mt-4 mb-6">Was für einen Kurs hast Du heute?</h2>
+        <div class="flex flex-col gap-4">
+          <v-btn v-for="course in courses" @click="form.course = course" size="x-large" rounded
+                 :color="course.color"
+                 :variant="form.course?.title!== course.title
+                 && form.course !== null
+                  ? 'outlined' : 'flat'"
+                 class="w-full"
+                 style="text-transform:none"
+                 :prepend-icon="course.icon">
+            {{ course.title }}
+          </v-btn>
+
+        </div>
+        <h2 v-if="form.course" class="text-2xl mt-8 mb-6">Und welche Zielgruppe ist heute anwesend?</h2>
+        <v-text-field
+            v-if="form.course"
+            autofocus
+            v-model="form.zielgruppe"
+            label="Zielgruppe"
+            placeholder="z.B. Dachbauer"
+            outlined
+            class="w-full"
+            style="text-transform:none"
+        />
+
+        <v-btn @click="step++" v-if="form.course" size="large"
+               color="#eb003c"
+               class="w-full"
+               rounded
+               prepend-icon="mdi-check">{{ form.zielgruppe.length > 0 ? 'Festlegen' : 'Keine Zielgruppe' }}
+        </v-btn>
+
+      </template>
+      <template v-else-if="step==1">
+        <h1 class="text-4xl mb-6">{{ form.course.title }}</h1>
+        <h2 class="text-2xl mt-4 mb-6">Welche Inseln sollen in den Fallbeispielen vorkommen?</h2>
+
+        <v-btn size="x-large" rounded
+               color="#eb003c"
+               class="w-full mb-4"
+               style="text-transform:none"
+               @click="zuFall"
+               prepend-icon="mdi-shuffle-variant">
+          Mir egal, Hauptsache bunt!
+        </v-btn>
+        <div class="grid grid-cols-2 gap-4 sm:grid-cols-3">
+          <v-btn v-for="(insel,i) in inseln"
+                 @click="inselFall(insel)"
+                 stacked
+                 rounded
+                 class="rounded-full h-auto aspect-square"
+                 v-show="insel.fall"
+                 :color="insel.color">
+            <img :src="'/quiz/inseln/i' +i+ '.svg'" class="w-16 h-16 aspect-square rounded-full"/>
+            <h1 class="font-semibold text-lg text-white" style="line-height:110%;text-transform:none;">{{
+                insel.name
+              }}</h1>
+          </v-btn>
+        </div>
+      </template>
+    </main>
+  </div>
+</template>
+<script setup>
+definePageMeta({layout: 'quiz'})
+import {inseln} from "~/composables/inseln"
+
+const step = ref(0)
+const courses = [
+  {
+    title: 'DVB Busfahrer',
+    chatgpt_title: 'Erste Hilfe Kurs für DVB Busfahrer',
+    short: 'DVB',
+    icon: 'mdi-bus',
+    color: '#e4b600'
+
+  },
+  {
+    title: 'Erste Hilfe Kurs',
+    chatgpt_title: 'Erste Hilfe Kurs nach DGUV 304-001',
+    short: 'EH',
+    icon: 'mdi-heart-pulse',
+    color: '#eb003c'
+  },
+  {
+    title: 'Schulsanitätsdienst',
+    chatgpt_title: 'Erste Hilfe Kurs erweitert auf Schulsanitätsdienst Niveau',
+    icon: 'mdi-school',
+    short: 'SSD',
+    color: '#00a0e4'
+  },
+  {
+    title: 'Sanitätshelfer',
+    chatgpt_title: 'Sanitätshelfer Qualifikation in 87 UE',
+    short: 'SanH',
+    icon: 'mdi-hospital-building',
+    color: '#818181'
+  }
+]
+const form = ref({
+  course: null,
+  zielgruppe: '',
+  insel: null,
+  fallauswahl: ''
+})
+
+// watch auf course, falls der dvb wird kannst du die zielgruppe auf "Busfahrer" setzen
+watch(() => form.value.course, (course) => {
+  if (course?.short === 'DVB') {
+    form.zielgruppe = 'Busfahrer'
+    step.value++
+  }
+})
+
+
+const inselFall = (insel) => {
+  form.insel = insel
+  form.fallauswahl = insel.fall
+  submit()
+}
+const zuFall = () => {
+  form.insel = null
+  form.fallauswahl = ''
+  submit()
+}
+
+const submit = () => {
+  console.log('form', form.value)
+  // const proxy prefix url
+  const url = 'https://cors.app4juh.de/'
+  // const api url
+  const api = 'https://p2.faktorxmensch.com/api/service/app4juh/fall'
+
+  // post there in the for of the post body being {kursart, insel, zielgruppe, fall}
+  fetch(api, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      kursart: form.value.course?.title,
+      insel: form.value.insel?.name,
+      zielgruppe: form.value.zielgruppe,
+      fall: form.value.fallauswahl
+    })
+  }).then(response => response.json())
+      .then(async data => {
+        console.log('Success:', data);
+        /* beispiel antwort
+        {
+    "diagnose_schlagwort": "Herzinfarkt",
+    "fallbeispiel_aufgabe": "Sie sind als Busfahrer für die DVB unterwegs. Plötzlich klagt ein Fahrgast über starke Schmerzen in der Brust und Atemnot. Er wirkt blass und schwitzt stark. Wie reagieren Sie?",
+    "fallbeispiel_aufloesung": "Der Fahrgast zeigt typische Anzeichen eines Herzinfarkts. Als Busfahrer im DVB - Erste Hilfe Kurs haben Sie gelernt, wie Sie bei einem Herzinfarkt vorgehen sollten. Sie stoppen den Bus an einer sicheren Stelle, stellen den Warnblinker an und informieren die Leitstelle über den Notfall. Sie bitten die anderen Fahrgäste, den Bus zu verlassen und rufen weitere Hilfe herbei. Da Sie als Busfahrer über erweiterte Erste-Hilfe-Kenntnisse verfügen, können Sie dem Fahrgast beim Platznehmen unterstützen und ihn dazu ermutigen, sich den Oberkörper zu lockern und tief durchzuatmen. Sie überwachen sein Bewusstsein und seine Atmung und bereiten sich darauf vor, bei einem Herzstillstand eine Herzdruckmassage durchzuführen, falls erforderlich. Sie nutzen die im Bus befindliche Ausrüstung, wie den Defibrillator, um im Ernstfall lebensrettende Maßnahmen einzuleiten. Sobald professionelle Hilfe eintrifft, übergeben Sie die weitere Versorgung des Patienten an das Rettungsteam.",
+    "success": true
+}
+         */
+        if (data.success) {
+          await Swal.fire({
+            title: 'Fallbeispiel',
+            html: data.fallbeispiel_aufgabe,
+            icon: 'success',
+            confirmButtonText: 'Auflösung anzeigen'
+          })
+
+          await Swal.fire({
+            title: data.diagnose_schlagwort,
+            html: data.fallbeispiel_aufloesung,
+            icon: 'success',
+            confirmButtonText: 'Fall beenden'
+          })
+        } else {
+          await Swal.fire({
+            title: 'Fehler',
+            html: 'Es ist ein Fehler aufgetreten. Bitte versuche es erneut!',
+            icon: 'error',
+            confirmButtonText: 'Okay'
+          })
+        }
+      })
+      .catch(async (error) => {
+        console.error('Error:', error);
+        await Swal.fire({
+          title: 'Fehler',
+          html: 'Es ist ein Fehler aufgetreten: ' + error.message,
+          icon: 'error',
+          confirmButtonText: 'OK'
+        })
+      });
+}
+
+</script>
