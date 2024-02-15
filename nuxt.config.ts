5bc2744e174a82528a11426ef4a5dea445dd3dee
Dominik <dominik.ziegenhagel@gmail.com>
2024-02-15 19:18:23 +0100

    title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     /* have favicon and mobile web app capable and title App4Juh*/
     app: {
         head: {
-            title: "Apps",
+            title: "App4Juh",
             meta: [
                 {charset: "utf-8"},
                 {name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"},
