d5537fa65afcb69c927c5c4771094bd7185e6543
Dominik <dominik.ziegenhagel@gmail.com>
2024-02-15 18:54:49 +0100

    app capabl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,23 +13,25 @@ export default defineNuxtConfig({
     },
     ssr: false,
     /* have favicon and mobile web app capable and title App4Juh*/
-    head: {
-        title: "Apps",
-        meta: [
-            {charset: "utf-8"},
-            {name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"},
-            {hid: "description", name: "description", content: "Apps für die Erste Hilfe Kurse der JUH"},
-            // full web app ios and android
-            {name: "mobile-web-app-capable", content: "yes"},
-            {name: "apple-mobile-web-app-capable", content: "yes"},
-            {name: "apple-mobile-web-app-status-bar-style", content: "black"},
-            {name: "apple-mobile-web-app-title", content: "App4Juh"},
-            // favicon
-            {name: "msapplication-TileImage", content: "/image/app4juh.jpg"},
+    app: {
+        head: {
+            title: "Apps",
+            meta: [
+                {charset: "utf-8"},
+                {name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"},
+                {hid: "description", name: "description", content: "Apps für die Erste Hilfe Kurse der JUH"},
+                // full web app ios and android
+                {name: "mobile-web-app-capable", content: "yes"},
+                {name: "apple-mobile-web-app-capable", content: "yes"},
+                {name: "apple-mobile-web-app-status-bar-style", content: "black"},
+                {name: "apple-mobile-web-app-title", content: "App4Juh"},
+                // favicon
+                {name: "msapplication-TileImage", content: "/image/app4juh.jpg"},
 
 
-        ],
-        link: [{rel: "icon", type: "image/x-icon", href: "/image/app4juh.jpg"}],
+            ],
+            link: [{rel: "icon", type: "image/x-icon", href: "/image/app4juh.jpg"}],
+        },
     },
 
     imports: {
